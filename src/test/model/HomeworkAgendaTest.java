e51a8ed2766703ce9c5822948033f83650fb3bb2
lujialin <mike020830@gmail.com>
2021-10-11 20:29:04 -0700

    made improvements

--- src/test/model/HomeworkAgendaTest.java
+++ src/test/model/HomeworkAgendaTest.java
@@ -72,6 +72,30 @@ class HomeworkAgendaTest {
         assertEquals("science", subject2);
     }
 
+    @Test
+    public void testDeleteHomeworkSmallAgenda() {
+        Homework homework = new Homework("math", "webwork");
+        hwAgenda.addHomework(homework);
+        hwAgenda.deleteHomework(homework);
+
+        assertEquals(0,hwAgenda.length());
+    }
+
+    @Test
+    public void testDeleteHomeworkFromLargeAgenda() {
+        Homework homework = new Homework("math", "webwork");
+        Homework homework1 = new Homework("science", "webwork");
+        Homework homework2 = new Homework("history", "webwork");
+        hwAgenda.addHomework(homework);
+        hwAgenda.addHomework(homework1);
+        hwAgenda.addHomework(homework2);
+
+        hwAgenda.deleteHomework(homework);
+
+        assertEquals(2,hwAgenda.length());
+        assertEquals("science", hwAgenda.get(0).getSubject());
+        assertEquals("history", hwAgenda.get(1).getSubject());
+    }
 
 
 
